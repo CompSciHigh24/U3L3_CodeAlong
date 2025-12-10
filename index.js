e9e06a78ec480fae1a9dc8634a660793542d9894
CompSciHigh24 <csh-github@compscihigh.org>
2025-12-10 08:11:52 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// Step 1: Install Express.js via the terminal command:
+// 'npm install express' or the shorthand version 'npm i express'
+
+// Step 2: Import the express module using 'require', name the variable express
+
+// Step 3: Create an instance of express() and name the variable 'app'
+
+// Step 4: Set up the server using 'app.listen'. 
+// Set the port is set to 3000. 
+// Leave the callback function empty for now.
+
+// Step 5: Start your server by running the index.js file
+// Notice how the terminal is running but nothing is being console logged. 
+// Stop your server by using the command ctrl + c
+
+// Step 6: Console log "Server is running!!!" inside the callback function. 
+// Restart your server
+
+// Step 7: Follow the URL pop up. 
+
+// Step 8: Go to the slides and add some of that code that you see. On your own, explore and try to see if you can get an HTML page to run.
